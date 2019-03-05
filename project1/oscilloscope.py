5fb436d4f1d47d4eb910744a61d7cb6b0eba791a
jose0796 <jmoran071996@gmail.com>
2019-03-05 11:48:30 -0400

    Most features are implemented. Time expansion feature not implemented.

--- project1/oscilloscope.py
+++ project1/oscilloscope.py
@@ -1,10 +1,10 @@
 #!python 3.x
+import os
 import sys 
 import serial 
 from com import *
 import numpy as np
 import math
-import random
 from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
@@ -13,12 +13,36 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+def showScale(scl):
+    if scl == 0:
+        return 0.3
+    elif scl == 1:
+        return 1
+    elif scl == 2:
+        return 3
+
+def scaleYAxis(scl): 
+    if scl== 0:
+        return 1
+    elif scl == 1:
+        return 3/10
+    elif scl == 2:
+        return 1/10
+
+def scaleTimeAxis(time=2):
+    return 2*(10**(time+1))
+
+def showDivScales(self,scale2: str,scale1: str) -> None:
+    self.axes.text(0.7,0.5,"channel 1: %.1f" % scale1)
+    self.axes.text(0.7,0.3,"channel 2: %.1f" % scale2)
+
 class MplCanvas(FigureCanvas):
     
     def __init__(self, parent=None, width=1, height=1, dpi=100):
         self.fig = plt.figure()
+        self.fig.subplots_adjust(left=0.1,right=0.9,top=0.9,bottom=0.1)
         FigureCanvas.__init__(self, self.fig)
-        #Figure(figsize=(width, height), dpi=dpi)
+        
         self.axes = self.fig.add_subplot(111)
         self.axes.set_yticklabels([])
         self.axes.set_xticklabels([])
@@ -26,23 +50,37 @@ class MplCanvas(FigureCanvas):
         self.canvas = FigureCanvas(self.fig)
         self.canvas.setParent(parent)
         self.canvas.move(60,80)
-        self.dataSerial = serial.Serial('/dev/ttyUSB0', baudrate=115200)
 
-    def plot(self):
-        self.update_figure()
-        
 
-    def update_figure(self):
+        self.timescale = np.linspace(start=0,stop=1,num=2000,endpoint=True)
+        self.xticks = np.linspace(0,1,10)
+        self.yticks = np.linspace(0,3,10)
+
+        self.dataSerial = serial.Serial('/dev/ttyUSB1', baudrate=115200)
+        self.axes.xaxis.set_ticks(self.xticks)
+        self.axes.yaxis.set_ticks(self.yticks)
+
+
+    def plot(self,ch1=0, ch2=0,time=2):
+        def _plot():
+            self.update_figure(ch1,ch2,time)
+        return _plot
+
+    def update_figure(self,ch1=0, ch2=0, time=2):
         self.axes.clear()
         channel1 = []
         channel2 = []
-        data =[]
-        timescale = np.linspace(start=0,stop=1,num=2000,endpoint=True)
-        while len(channel1) != len(timescale):
+        data = []
+        while len(channel1) != len(self.timescale):
             data = startReceiving(self.dataSerial)
-            channel1.append(convert(data[0]))
-            channel2.append(convert(data[1]))
-        self.axes.plot(timescale,channel1)
+            channel1.append(scaleYAxis(ch1)*convert(data[0]))
+            channel2.append(scaleYAxis(ch2)*convert(data[1]))
+        ts = scaleTimeAxis(time)
+        
+        self.axes.plot(self.timescale[0:ts],channel1[0:ts])
+        showDivScales(self,showScale(ch2),showScale(ch1))
+        self.axes.xaxis.set_ticks(self.xticks)
+        self.axes.yaxis.set_ticks(self.yticks)
         self.axes.set_xlim([0.0, 1.0])
         self.axes.set_ylim([0.0, 3.0])
         self.axes.set_xticklabels([])
@@ -67,54 +105,59 @@ class Window(QMainWindow):
 
         self.canvas = MplCanvas(self)
 
+        self.timer = None
+
         #window config
         self.title = "Demoqe Project"
         self.top = 100
         self.left = 100
         self.width = 1000 
-        self.height = 700 
+        self.height = 600 
         
         #push button config 
         button = QPushButton("Start", self) 
-        button.move(800,300)
-        button.clicked.connect(self.plot)
+        button.move(790,300)
+        button.clicked.connect(self.__plot(0))
 
         #tool tip for push button 
-        self.setToolTip("Hello world")
+        # self.setToolTip("Hello world")
         self.setWindowIcon(QtGui.QIcon("icon.png"))
         
         #slider for channel 1
-        self.text   = QLabel("Channel 1", self)
+        self.text = QLabel("Channel 1", self)
         self.text.move(800,75)
 
-        self.slider = QSlider(Qt.Horizontal, self)
-        self.slider.setGeometry(750,100,170,20)
-        self.slider.setMinimum(0)
-        self.slider.setMaximum(2)
-        self.slider.setTickPosition(QSlider.TicksAbove)
-        self.slider.setTickInterval(1)
+        self.channel1 = QSlider(Qt.Horizontal, self)
+        self.channel1.setGeometry(750,100,170,20)
+        self.channel1.setMinimum(0)
+        self.channel1.setMaximum(2)
+        self.channel1.setTickPosition(QSlider.TicksAbove)
+        self.channel1.setTickInterval(1)
+        self.channel1.valueChanged.connect(self.channel1ChangedValue)
 
         #slider for channel 2
-        self.text   = QLabel("Channel 2", self)
+        self.text = QLabel("Channel 2", self)
         self.text.move(800,125)
 
-        self.slider = QSlider(Qt.Horizontal, self)
-        self.slider.setGeometry(750,150,170,20)
-        self.slider.setMinimum(0)
-        self.slider.setMaximum(2)
-        self.slider.setTickPosition(QSlider.TicksAbove)
-        self.slider.setTickInterval(1)
+        self.channel2 = QSlider(Qt.Horizontal, self)
+        self.channel2.setGeometry(750,150,170,20)
+        self.channel2.setMinimum(0)
+        self.channel2.setMaximum(2)
+        self.channel2.setTickPosition(QSlider.TicksAbove)
+        self.channel2.setTickInterval(1)
+        self.channel2.valueChanged.connect(self.channel2ChangedValue)
 
         #position 
-        self.text   = QLabel("Position", self)
+        self.text = QLabel("Position", self)
         self.text.move(800,175)
 
-        self.slider = QSlider(Qt.Horizontal, self)
-        self.slider.setGeometry(750,200,170,20)
-        self.slider.setMinimum(0)
-        self.slider.setMaximum(2)
-        self.slider.setTickPosition(QSlider.TicksAbove)
-        self.slider.setTickInterval(1)
+        self.position = QSlider(Qt.Horizontal, self)
+        self.position.setGeometry(750,200,170,20)
+        self.position.setMinimum(0)
+        self.position.setMaximum(2)
+        self.position.setTickPosition(QSlider.TicksAbove)
+        self.position.setTickInterval(1)
+        self.position.valueChanged.connect(self.positionChanged)
 
         self.InitWindow()
 
@@ -125,12 +168,33 @@ class Window(QMainWindow):
 
     def fileQuit(self): 
         self.close()
-    
-    def plot(self):
+        
+    def channel1ChangedValue(self):
+        self.plot(self.channel1.value(), self.channel2.value())    
+
+    def channel2ChangedValue(self):
+        self.plot(self.channel1.value(), self.channel2.value()) 
+
+    def positionChanged(self):
+        self.plot(self.channel1.value(),self.channel2.value(),self.position.value())
+
+    def __plot(self,option=1):
+        def ___plot():
+            if option == 0:
+                self.plot()
+
+        return ___plot
+
+
+    def plot(self,opt1=0, opt2=0,opt3=2):
         ''' plot some random stuff '''
-        timer = QtCore.QTimer(self)
-        timer.timeout.connect(self.canvas.plot)
-        timer.start(1)
+        if self.timer: 
+            self.timer.stop()
+            self.timer.deleteLater()
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.canvas.plot(opt1,opt2,opt3))
+        self.timer.start(116)
+
         
 
 
