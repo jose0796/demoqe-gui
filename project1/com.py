d586f9c605bd87df9e302dbd610c712fdcd7eeb0
jose0796 <jmoran071996@gmail.com>
2019-03-05 23:08:41 -0400

    Emergency pushing

--- project1/com.py
+++ project1/com.py
@@ -1,21 +1,36 @@
+#!python 3.x
+#################################################
+## Author       : Jose Moran
+## Github       : jose0796
+## email        : jmoran071996@gmail.com
+## file         : com.py 
+## Description  : Library for digital and analog 
+## unframing and processing for MC9S08QE128 uC
+#################################################
 
-def convert(data, max=3, min=0, bitnum=12): 
+
+def convertDigital(data, max=3, min=0, bitnum=1):
+    data_converted = int(data*(max))
+    return data_converted
+
+
+def convertAnalog(data, max=3, min=0, bitnum=12): 
     data_converted = float(data*(max-min)/(2**12))
     return data_converted
 
 
-def unframeData(frame,channel=1,digital=0):
+def unframeData(frame,channel=1,digital=False):
     
     if channel == 1:
-        if digital == 0:
+        if not digital :
             return ((frame[0] & 0x3f) << 6 ) | (frame[1] & 0x3f)
         else: 
-            return (frame[0] & 0xb0) >> 6
+            return (frame[0] & 0xb0) >> 7
     elif channel == 2:
-        if digital == 0:
+        if not digital:
             return ((frame[2] & 0x3f) << 6 ) | (frame[3] & 0x3f)
         else:
-            return (frame[1] & 0xb0) >> 6 
+            return (frame[1] & 0xb0) >> 7
 
 
 def synchronize(dataSerial):
@@ -29,11 +44,14 @@ def synchronize(dataSerial):
 def startReceiving(dataSerial):
     frames = dataSerial.read(4)
     if ((frames[0] & 0x8f) >> 7) == 0 and ((frames[1] & 0x8f) >> 7) == 1 and ((frames[2] & 0x8f) >> 7) == 1 and ((frames[3] & 0x8f) >> 7) == 1 : 
-        channel1 = unframeData(frames)
-        channel2 = unframeData(frames,2)
-        return channel1,channel2
+        channelA1 = unframeData(frames)
+        channelA2 = unframeData(frames,2)
+        channelD1 = unframeData(frames,1,True)
+        channelD2 = unframeData(frames,2,True)
+
+        return channelA1,channelA2,channelD1,channelD2
     else: 
         synchronize(dataSerial)
-        return 0,0
+        return 0,0,0,0
 
 
