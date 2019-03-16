e7370dd1c1899151525fc1a8919c731f056982b6
jose0796 <jmoran071996@gmail.com>
2019-03-16 00:09:36 -0400

    Improving commenting

--- project1/com.py
+++ project1/com.py
@@ -9,6 +9,12 @@
 #################################################
 
 def openPort(ports,baudrate=115200):
+
+    """ Opens proper port from a given list
+        :returns:
+            A serial.Serial instance for the proper port
+    """
+
     import serial
     for port in ports:
         s = serial.Serial(port,baudrate=baudrate,timeout=0.1)
@@ -17,6 +23,14 @@ def openPort(ports,baudrate=115200):
         
 
 def listPorts(platform):
+
+    """ Lists ports for specific platform
+        :returns:
+            A list of ports (strings)
+
+    """
+
+
     import glob 
     import serial
 
@@ -29,18 +43,19 @@ def listPorts(platform):
     else:
         raise EnvironmentError('Unsuported Platform')
     
-    result = []
+    portList = []
     for port in ports:
         try: 
             s = serial.Serial(port,baudrate=115200,timeout=1)
             s.close()
-            result.append(port)
+            portList.append(port)
         except (OSError, serial.SerialException):
             pass
     
-    return result 
+    return portList
 
 def convertDigital(data, max=3):
+
     data_converted = int(data*(max))
     return data_converted
 
@@ -51,7 +66,10 @@ def convertAnalog(data, max=3, min=0, bitnum=12):
 
 
 def unframeData(frame,channel=1,digital=False):
-    
+
+    """ Unframe data received from serial port based on defined protocol 
+    """
+
     if channel == 1:
         if not digital :
             return ((frame[0] & 0x3f) << 6 ) | (frame[1] & 0x3f)
@@ -66,14 +84,16 @@ def unframeData(frame,channel=1,digital=False):
 
 
 def synchronize(dataSerial):
-    
+    """ Synchronize data received from serial port
+    """
     while(True):
         b = dataSerial.read(1)
         if ((b[0] & 0x8f) >> 7 ) == 0x00:
             garbage = dataSerial.read(3)
             break
 
-def startReceiving(dataSerial):
+def receiveData(dataSerial):
+    
     frames = dataSerial.read(4)
     if ((frames[0] & 0x8f) >> 7) == 0 and ((frames[1] & 0x8f) >> 7) == 1 and ((frames[2] & 0x8f) >> 7) == 1 and ((frames[3] & 0x8f) >> 7) == 1 : 
         channelA1 = unframeData(frames)
